5e1c700908583beba4646f1a80e7c9383484e10b
halhee <119079158+halhee@users.noreply.github.com>
2023-05-23 23:01:26 +0200

    Update Checkers.py
    Elle charge d'abord les types d'éléments à partir du fichier Excel qui sont utilisés pour filtrer les éléments dans le fichier IFC.
    Ensuite, elle charge les Psets requis (Property Sets) et les paramètres à partir du fichier Excel. Ces derniers sont utilisés pour vérifier si les éléments du fichier IFC contiennent les bons Psets et paramètres.
    Le fichier IFC est ensuite ouvert et les éléments sont filtrés en fonction des types d'éléments chargés à partir du fichier Excel.
    Pour chaque type d'élément, le code vérifie si l'élément a les bons Psets et paramètres. Si un Pset ou un paramètre est manquant, ou si la valeur d'un paramètre n'est pas du bon type, il est noté dans le fichier Excel de sortie.
    Le fichier Excel de sortie est ensuite enregistré et renvoyé à l'utilisateur.

--- Checkers.py
+++ Checkers.py
@@ -62,86 +62,91 @@ def gray_empty_cells(worksheet):
             if cell.value is None or cell.value == "":
                 cell.fill = gray_fill
 
-def get_building_storey(ifc_file, element):
-    for rel in element.ContainedInStructure:
-        if rel.RelatingStructure.is_a("IfcBuildingStorey"):
-            return rel.RelatingStructure
-        elif rel.RelatingStructure.is_a("IfcBuilding") or rel.RelatingStructure.is_a("IfcSite"):
-            return None
-        else:
-            return get_building_storey(ifc_file, rel.RelatingStructure)
+def get_building_storey(ifc_file, global_id):
+    element = ifc_file.by_guid(global_id)
+    if element is not None:
+        for rel in element.ContainedInStructure:
+            if rel.RelatingStructure.is_a("IfcBuildingStorey"):
+                return rel.RelatingStructure.Name if rel.RelatingStructure.Name else ""
+            elif rel.RelatingStructure.is_a("IfcBuilding") or rel.RelatingStructure.is_a("IfcSite"):
+                return ""
+            else:
+                return get_building_storey(ifc_file, rel.RelatingStructure)
+    return ""
 
 def process_files(temp_dir, ifc_file_path, excel_file_path, output_file_path):
     element_types = load_element_types(excel_file_path)
     required_psets_and_params = load_required_psets_and_params(excel_file_path)
 
-    # Charger le fichier IFC
+    # Load the IFC file
     ifc_file = ifcopenshell.open(ifc_file_path)
 
-    # Obtenir la liste des éléments et filtrer par type d'élément
+    # Get the list of elements and filter by element type
     elements = ifc_file.by_type("IfcElement")
     filtered_elements = [e for e in elements if e.is_a() in element_types]
 
-    # Création d'un classeur Excel de sortie
+    # Creating an output Excel workbook
     output_workbook = Workbook()
-    output_workbook.remove(output_workbook.active)  # Supprimer la feuille active par défaut
+    output_workbook.remove(output_workbook.active)  # Remove the default active sheet
 
-    # Création de la feuille "Overview"
-    overview_sheet = output_workbook.create_sheet("Overview")
-    overview_sheet.append(["IFC Class", "Pset Name", "Percentage of Correct Properties"])
+    pset_sheets = {}  # A dictionary to store the sheets created for each PSet
+    # Get the 3D model name from the IFC file path
+    model_name = os.path.basename(ifc_file_path)
 
     for ifc_class, psets in required_psets_and_params.items():
         for pset_name in psets:
-            # Créer une nouvelle feuille pour chaque PSet s'il n'existe pas encore
-            pset_sheet = output_workbook.create_sheet(f"{ifc_class}_{pset_name}")
-            pset_sheet.append(["IFC Class", "Element GlobalId", "Pset Name", "Parameter Name", "Floor", "Value", "Status"])
-
-            num_elements = 0
-            num_correct_properties = 0
+            # Create a new sheet for each PSet if it doesn't exist yet
+            if pset_name not in pset_sheets:
+                pset_sheets[pset_name] = output_workbook.create_sheet(f"Results_{pset_name}")
+                pset_sheets[pset_name].append(["IFC Class", "3D Model", "Element GlobalId", "Pset Name", "Parameter Name", "Floor", "Value", "Status"])
 
             for element in filtered_elements:
                 if element.is_a() == ifc_class:
                     pset = ifcopenshell.util.element.get_psets(element).get(pset_name, None)
 
-                    # Obtenir l'étage de l'élément
-                    floor = get_building_storey(ifc_file, element)
-                    floor_name = floor.Name if floor else "N/A"
-
                     for param_name, param_type_str in required_psets_and_params[ifc_class][pset_name].items():
                         param_type = str_to_type(param_type_str)
                         if pset is None:
-                            pset_sheet.append([ifc_class, element.GlobalId, pset_name, param_name, floor_name, "", "PSet missing"])
+                            pset_sheets[pset_name].append([element.is_a(), model_name, element.GlobalId, pset_name, param_name, get_building_storey(ifc_file, element.GlobalId), "", "PSet missing"])
                         else:
                             value = pset.get(param_name, None)
                             if value is None:
-                                pset_sheet.append([ifc_class, element.GlobalId, pset_name, param_name, floor_name, "", "Param missing"])
+                                pset_sheets[pset_name].append([element.is_a(), model_name, element.GlobalId, pset_name, param_name, get_building_storey(ifc_file, element.GlobalId), "", "Param missing"])
                             else:
-                                # Vérifier si la propriété est correcte
-                                expected_type = str_to_type(param_type_str)
-                                try:
-                                    # Tentative de conversion de la valeur à son type attendu
-                                    converted_value = expected_type(value)
-                                    pset_sheet.append([ifc_class, element.GlobalId, pset_name, param_name, floor_name, value, "OK"])
-                                    num_correct_properties += 1
-                                except ValueError:
-                                    pset_sheet.append([ifc_class, element.GlobalId, pset_name, param_name, floor_name, value, "Incorrect"])
+                                pset_sheets[pset_name].append([element.is_a(), model_name, element.GlobalId, pset_name, param_name, get_building_storey(ifc_file, element.GlobalId), value, "OK"])
 
-                    num_elements += 1
+    # Create an "Overview" sheet
+    overview_sheet = output_workbook.create_sheet("Overview")
+    overview_sheet.append(["IFC Class", "3D Model", "Pset Name", "Correct Property set", "Correct parameters", "Correct values", "Percentage of Correct values"])
 
-            if num_elements > 0:
-                percentage_correct = (num_correct_properties / (len(required_psets_and_params[ifc_class][pset_name]) * num_elements)) * 100
-            else:
-                percentage_correct = 0
+    # Count OK params, missing params, and missing PSets for each sheet
+    for sheet_name, sheet in pset_sheets.items():
+        ok_params = 0
+        missing_params = 0
+        missing_ps = 0
+        total_params = 0
+        for row in sheet.iter_rows(min_row=2, values_only=True):  # skip header row
+            if row[7] == "OK":
+                ok_params += 1
+            elif row[7] == "Param missing":
+                missing_params += 1
+            elif row[7] == "PSet missing":
+                missing_ps += 1
+            total_params += 1
+        correct_ps = total_params - missing_ps
+        correct_params = total_params - missing_params
+        percentage_correct = (ok_params / total_params) * 100 if total_params > 0 else 0
+        overview_sheet.append([row[0], model_name, sheet_name, correct_ps, correct_params, ok_params, f"{percentage_correct:.2f}"])
 
-            overview_sheet.append([ifc_class, pset_name, f"{percentage_correct:.2f}"])
+    # Move the "Overview" sheet to the first position
+    output_workbook._sheets.sort(key=lambda ws: ws.title!='Overview')
 
-    # Appliquer la mise en forme
+    # Apply the formatting
     for sheet in output_workbook:
         gray_empty_cells(sheet)
 
-    # Enregistrer le classeur de sortie
+    # Save the output workbook
     output_workbook.save(output_file_path)
-
 
 
 @app.route('/')
@@ -161,13 +166,10 @@ def upload():
             ifc_file.save(ifc_file_path)
             excel_file.save(excel_file_path)
 
-            # Extraire le nom de base du fichier IFC sans extension
-            ifc_base_name = os.path.splitext(ifc_filename)[0]
-
             with tempfile.TemporaryDirectory() as temp_dir:
-                output_file_path = os.path.join(temp_dir, f'output_{ifc_base_name}.xlsx')
+                output_file_path = os.path.join(temp_dir, f'output_{os.path.splitext(ifc_filename)[0]}.xlsx')
                 process_files(temp_dir, ifc_file_path, excel_file_path, output_file_path)
-                return send_file(output_file_path, as_attachment=True, download_name=f'output_{ifc_base_name}.xlsx')
+                return send_file(output_file_path, as_attachment=True, download_name=f'output_{os.path.splitext(ifc_filename)[0]}.xlsx')
 
     return redirect(url_for('index'))
 
